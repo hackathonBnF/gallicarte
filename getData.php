5919a6a8a23381ac8cca3972fe06afef2bf7a3a6
PhilippeGambette <philippe.gambette@laposte.net>
2016-11-20 09:14:16 +0100

    Now allows to retrieve multi-page queries

--- getData.php
+++ getData.php
@@ -1,12 +1,15 @@
 <?php
 //Retrieves the results from Gallica
+$startRecord=1;
+if(isset($_GET["startRecord"])){
+   $startRecord=$_GET["startRecord"];
+}
 if(isset($_GET["q"])){
    $query = $_GET["q"];
-   $fullQuery = 'http://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2&maximumRecords=10&startRecord=1&query='.str_replace("\"","%22",str_replace(" ","%20",$query));
+   $fullQuery = 'http://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2&maximumRecords=50&startRecord='.$startRecord.'&query='.str_replace('\%22',"%22",str_replace('"',"%22",str_replace(" ","%20",$query)));
    //echo $fullQuery;
    $homepage = file_get_contents($fullQuery);
    //text%20any%20%22mar%C3%A2tre%22
    echo $homepage;
 }
-
 ?>
